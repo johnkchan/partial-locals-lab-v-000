7fd8690cbc9f38825b81798f9128c7d2c69204b3
Jonathan Chan <johnkchan252@gmail.com>
2019-02-19 06:37:30 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,7 +13,7 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
-  
+
   def self.search(query)
     if query.present?
       where("name like ?", "%#{query}%")
